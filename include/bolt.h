494d3cf38e7e56c58fbfec2caeaf04a972670919
Ankur Seth <iankurseth@gmail.com>
2020-10-21 03:11:28 +0530

    Suffixed non-pointer types with _t

--- include/bolt.h
+++ include/bolt.h
@@ -3,67 +3,72 @@
 
 #define BOLT_MAX_NAME_LENGTH 128
 
-struct Bolt;
-struct BoltAction;
-struct BoltExperiment;
-struct BoltMilestone;
+struct Bolt_t;
+struct BoltAction_t;
+struct BoltExperiment_t;
+struct BoltMilestone_t;
 
-typedef struct Bolt Bolt;
-typedef struct BoltAction BoltAction;
-typedef struct BoltExperiment BoltExperiment;
-typedef struct BoltMilestone BoltMilestone;
+typedef struct Bolt_t Bolt_t;
+typedef struct BoltAction_t BoltAction_t;
+typedef struct BoltExperiment_t BoltExperiment_t;
+typedef struct BoltMilestone_t BoltMilestone_t;
+
+typedef struct Bolt_t *Bolt;
+typedef struct BoltAction_t *BoltAction;
+typedef struct BoltExperiment_t *BoltExperiment;
+typedef struct BoltMilestone_t *BoltMilestone;
 
 
-struct Bolt {
+struct Bolt_t {
 	char name[BOLT_MAX_NAME_LENGTH];
 
-	BoltAction *actions;
+	BoltAction actions;
 	int actions_count;
 	int actions_capacity;
 
-	BoltExperiment *experiments;
+	BoltExperiment experiments;
 	int experiments_count;
 	int experiments_capacity;
 
-	BoltMilestone *milestones;
+	BoltMilestone milestones;
 	int milestones_count;
 	int milestones_capacity;
 };
 
-struct BoltAction {
-	Bolt *bolt;
+struct BoltAction_t {
+	Bolt bolt;
 	char name[BOLT_MAX_NAME_LENGTH];
 	long iterations;
 };
 
-struct BoltExperiment {
-	Bolt *bolt;
+struct BoltExperiment_t {
+	Bolt bolt;
 	char name[BOLT_MAX_NAME_LENGTH];
 	long iterations;
 };
 
-struct BoltMilestone {
-	Bolt *bolt;
+struct BoltMilestone_t {
+	Bolt bolt;
 	char name[BOLT_MAX_NAME_LENGTH];
 };
 
 
-Bolt *bolt_new();
+Bolt bolt_new();
 
-void bolt_add_action (Bolt *bolt, char *name, long iterations);
-void bolt_add_experiment (Bolt *bolt, char *name, long iterations);
-void bolt_add_milestone (Bolt *bolt, char *name);
+void bolt_add_action (Bolt bolt, char *name, long iterations);
+void bolt_add_experiment (Bolt bolt, char *name, long iterations);
+void bolt_add_milestone (Bolt bolt, char *name);
 
-void bolt_run(Bolt *bolt);
-void bolt_run_actions(Bolt *bolt);
-void bolt_run_experiments(Bolt *bolt);
+void bolt_run(Bolt bolt);
+void bolt_run_actions(Bolt bolt);
+void bolt_run_experiments(Bolt bolt);
 
-void bolt_print(Bolt *bolt);
-void bolt_print_actions(Bolt *bolt);
-void bolt_print_experiments(Bolt *bolt);
-void bolt_print_milestones(Bolt *bolt);
+void bolt_print(Bolt bolt);
+void bolt_print_actions(Bolt bolt);
+void bolt_print_experiments(Bolt bolt);
+void bolt_print_milestones(Bolt bolt);
 
-Bolt *bolt_delete(Bolt *bolt);
+Bolt bolt_delete(Bolt bolt);
 
 
 
