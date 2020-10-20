7a82a1fc9166a6b651a9ecc45b239ee199d45456
Ankur Seth <iankurseth@gmail.com>
2020-10-21 03:33:22 +0530

    Added macro: BOLT_SAVE_GENERIC

--- lib/bolt.c
+++ lib/bolt.c
@@ -2,26 +2,30 @@
 
 #include <stdio.h>
 
-void bolt_save_csv(Bolt bolt, char *filename) {
-	FILE *fp = fopen(filename, "w");
-	if (fp == NULL) {
-		return;
-	}
-	char *text = bolt_get_csv(bolt);
-	int result = fputs(text, fp);
+
+
+#define BOLT_SAVE_GENERIC(bolt_get_func) FILE *fp = fopen(filename, "w"); \
+	if (fp == NULL) return; \
+	char *text = bolt_get_func(bolt); \
+	int result = fputs(text, fp); \
 	fclose(fp);
+
+void bolt_save_csv(Bolt bolt, char *filename) {
+	BOLT_SAVE_GENERIC(bolt_get_csv);
 }
 
 void bolt_save_json(Bolt bolt, char *filename) {
-	//
+	BOLT_SAVE_GENERIC(bolt_get_json);
 }
 
 void bolt_save_html(Bolt bolt, char *filename) {
-	//
+	BOLT_SAVE_GENERIC(bolt_get_html);
 }
 
 void bolt_save_xml(Bolt bolt, char *filename) {
-	//
+	BOLT_SAVE_GENERIC(bolt_get_xml);
 }
+
+#undef BOLT_SAVE_GENERIC
 
 
