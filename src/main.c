4e2ea91170b8c75faebcad2c8aea9ab7f08fac26
Ankur Seth <iankurseth@gmail.com>
2020-10-21 07:44:45 +0530

    Added fluff to do_some_stuff()

--- src/main.c
+++ src/main.c
@@ -4,14 +4,17 @@
 void
 do_some_stuff ()
 {
-	//
+	int num = 0;
+	for (int i = 0; i < 1e6; ++i) {
+		num++;
+	}
 }
 
 int main (int argc, char const *argv[]) {
 	BOLT_INIT("Bolt Internal");
 	BOLT_MILESTONE("Start");
 	BOLT_ACTION(do_some_stuff, 1000);
-	BOLT_EXPERIMENT(do_some_stuff, 1000);
+	BOLT_EXPERIMENT(do_some_stuff, 10);
 	BOLT_MILESTONE("End");
 	BOLT_RUN;
 	BOLT_CLEAN;
